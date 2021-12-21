f1b3a159cb1c0ff80c7f1748629350451d2b6582
Jairo Navarro <jnavarr5@ucsc.edu>
2021-12-21 15:01:46 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Genome Browser FAQ'
+copyright = '2021, UCSC'
+author = 'Jairo'
 
 release = '0.1'
 version = '0.1.0'
