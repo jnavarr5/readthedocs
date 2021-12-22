4bdb65f4c3435e0cd431437c114f1c70a8945538
Jairo Navarro <jnavarr5@ucsc.edu>
2021-12-22 10:13:02 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,10 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = {
+    'sphinx_rtd_theme',
+    'titles_only': True
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
